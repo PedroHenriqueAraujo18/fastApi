ca88f8ec354e56710a2f0f810e6e28e24b5ee985
pedro <troopper.san@gmail.com>
2025-07-04 09:05:28 -0300

    Estudos de conexões com banco de dados usando SQLALCHEMY

--- SQLACHEMY/banco.py
+++ SQLACHEMY/banco.py
@@ -1,8 +1,18 @@
 from sqlalchemy import create_engine
 
-engine = create_engine(#Padrão de arquitetura conhecido como Factory
-    'postgresql+psycopg://...'
+engine_pg = create_engine(#Padrão de arquitetura conhecido como Factory
+    'postgresql+psycopg://...',
+    echo=True
 )
 
-print(f"{engine}")
-print(engine.dialect)
+engine_sql = create_engine(
+    'sqlite://',
+    echo=True
+)
+
+pg_con = engine_pg.connect()
+sqlite_con = engine_sql.connect()
+
+print(pg_con.connection.dbapi_connection)
+pg_con.close()
+sqlite_con.close()
